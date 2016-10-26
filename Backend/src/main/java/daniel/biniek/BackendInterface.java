c12c5470b2080d4a61d0f13388ab8c98810ed58f
daniel <danbiniek@gmail.co>
2016-10-27 01:21:44 +0200

    v1.2.1 ref

--- Backend/src/main/java/daniel/biniek/BackendInterface.java
+++ Backend/src/main/java/daniel/biniek/BackendInterface.java
@@ -5,15 +5,18 @@ import daniel.biniek.product.ProductOb;
 import daniel.biniek.product.ProductSender;
 import org.apache.activemq.ActiveMQConnection;
 import org.apache.activemq.ActiveMQConnectionFactory;
-import org.apache.activemq.broker.BrokerService;
 
 import javax.jms.*;
+import javax.jms.Queue;
 import java.net.ServerSocket;
-import java.util.Enumeration;
+import java.util.*;
 
 public class BackendInterface implements Runnable {
 
     private static String threadName;
+    private static List<ProductOb> sell = new ArrayList<>();
+    private static List<ProductOb> buy = new ArrayList<>();
+    private static List<ProductOb> all = new ArrayList<>();
 
     public static void main(String arg[]) throws Exception {
         ServerSocket socket = new ServerSocket(0);
@@ -60,7 +63,10 @@ public class BackendInterface implements Runnable {
     }
 
     public static String receiveMessage(String backNo) throws JMSException {
-        ActiveMQConnection connection;
+        Map<String, String> productMap = new HashMap<>();
+        ActiveMQConnection connection = null;
+        QueueSession session = null;
+        QueueBrowser browser = null;
         Queue queue;
         String perf = new String();
         try {
@@ -69,10 +75,10 @@ public class BackendInterface implements Runnable {
             factory.setTrustAllPackages(true);
             connection = (ActiveMQConnection) factory.createConnection();
 
-            QueueSession session = connection.createQueueSession(true, Session.CLIENT_ACKNOWLEDGE);
+            session = connection.createQueueSession(true, Session.CLIENT_ACKNOWLEDGE);
             queue = session.createQueue(QueueCode.MT_TO_BACK_QUEUE.toString() + "." + backNo);
             connection.start();
-            QueueBrowser browser = session.createBrowser(queue);
+            browser = session.createBrowser(queue);
             Enumeration<?> messagesInQueue = browser.getEnumeration();
 
             while (messagesInQueue.hasMoreElements()) {
@@ -82,6 +88,27 @@ public class BackendInterface implements Runnable {
                     String[] parts = textMessage.getText().split(";");
                     perf = "Method: " + parts[1] + ", product: " + parts[0];
                     System.out.println("Method: " + parts[1] + ", product: " + parts[0] + ", amount: " + parts[3] + ", price: " + parts[2]);
+                    productMap.put("name", parts[0]);
+                    productMap.put("price", parts[2]);
+                    productMap.put("amount", parts[3]);
+
+                    ProductOb product = generateProduct(productMap);
+
+                    if(parts[1].equals("Sell") && !sell.contains(product) && !all.contains(product)){
+                        product.setId(null);
+                        sell.add(product);
+                    }else if(parts[1].equals("Buy") && !buy.contains(product)){
+                        buy.add(product);
+                        MessageConsumer messageConsumer = session.createConsumer(queue);
+                        messageConsumer.receive();
+
+                    }
+
+                    if(!all.contains(product)){
+                        all.add(product);
+                    }
+
+                    performTransaction();
                 }
             }
 
@@ -90,8 +117,54 @@ public class BackendInterface implements Runnable {
         } finally {
            /* connection.stop();
             connection.destroyDestination(ActiveMQDestination.transform(queue));*/
+            session.commit();
+            browser.close();
+            connection.close();
            return perf;
         }
+    }
+
+    private static void performTransaction() {
+        if(!sell.isEmpty() && !buy.isEmpty()){
+            for (ProductOb s : sell){
+                for (ProductOb b : buy){
+                    if (canBuy(s, b)) {
+                        buy(s, b);
+                    }
+                }
+            }
+        }
+    }
+
+    private static void buy(ProductOb s, ProductOb b) {
+        System.out.println("Product buy. Name: " + b.getName() + " price: " + s.getPrice() + " amount: " + s.getAmount());
+        preapareNewValues(s, b);
+    }
+
+    private static void preapareNewValues(ProductOb s, ProductOb b) {
+        ProductOb newSellVal = new ProductOb();
+        newSellVal.setAmount(s.getAmount() - b.getAmount());
+        newSellVal.setName(s.getName());
+        newSellVal.setPrice(s.getPrice());
+
+        sell.add(newSellVal);
+        buy.remove(b);
+        sell.remove(s);
+    }
+
+    private static boolean canBuy(ProductOb s, ProductOb b) {
+        return s.getName().equals(b.getName())
+                && s.getAmount() >= b.getAmount()
+                && s.getPrice() <= b.getPrice();
+    }
+
+    private static ProductOb generateProduct(Map<String, String> productMap) {
+        ProductOb newProduct = new ProductOb();
+        newProduct.setName(productMap.get("name"));
+        newProduct.setAmount(Long.parseLong(productMap.get("amount")));
+        newProduct.setPrice(Double.parseDouble(productMap.get("price")));
+
+        return newProduct;
     }
 
     @Override
--- Backend/src/main/java/daniel/biniek/product/ProductOb.java
+++ Backend/src/main/java/daniel/biniek/product/ProductOb.java
@@ -6,9 +6,12 @@ public class ProductOb implements Serializable{
 
     String name;
     Long id;
-    float price;
+    Double price;
     String backName;
     Long amount;
+
+    public ProductOb() {
+    }
 
     public ProductOb(String name, Long id) {
         this.name = name;
@@ -31,11 +34,11 @@ public class ProductOb implements Serializable{
         this.id = id;
     }
 
-    public float getPrice() {
+    public Double getPrice() {
         return price;
     }
 
-    public void setPrice(float price) {
+    public void setPrice(Double price) {
         this.price = price;
     }
 
@@ -64,5 +67,30 @@ public class ProductOb implements Serializable{
                 ", backName='" + backName + '\'' +
                 ", amount=" + amount +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ProductOb productOb = (ProductOb) o;
+
+        if (name != null ? !name.equals(productOb.name) : productOb.name != null) return false;
+        if (id != null ? !id.equals(productOb.id) : productOb.id != null) return false;
+        if (price != null ? !price.equals(productOb.price) : productOb.price != null) return false;
+        if (backName != null ? !backName.equals(productOb.backName) : productOb.backName != null) return false;
+        return amount != null ? amount.equals(productOb.amount) : productOb.amount == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (id != null ? id.hashCode() : 0);
+        result = 31 * result + (price != null ? price.hashCode() : 0);
+        result = 31 * result + (backName != null ? backName.hashCode() : 0);
+        result = 31 * result + (amount != null ? amount.hashCode() : 0);
+        return result;
     }
 }
